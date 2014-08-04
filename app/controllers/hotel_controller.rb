b12559a38b686c84b13e8a64729f4707ab7bbfb2
Trevor <tmwagner66@gmail.com>
2014-08-03 23:27:45 -0400

    updating styling and redo-ing migrations to clean up the app

--- app/controllers/hotel_controller.rb
+++ app/controllers/hotel_controller.rb
@@ -1,2 +1,83 @@
 class HotelController < ApplicationController
+
+
+  private
+
+  def hotel_attributes
+      params.require(:hotel).permit(:hotel_name,:hotel_street,:hotel_city,:hotel_state,:hotel_zip,:hotel_phone,
+                                    :hotel_email,:hotel_lat,:hotel_lng,:hotel_image_url,:hotel_pets,
+                                    :hotel_pet_fee,:hotel_smoking,:hotel_smoking_fee,:hotel_type,
+                                    :hotel_region,:hotel_parking_fee )
+  end
+
+
+
+  # SHOW ALL HOTELS ON DASHBOARD
+  def index
+    @hotels = Hotel.all().order(:hotel_type)
+    @hotelCategories = Hotel.get_hotel_categories
+  end
+
+
+  # SHOW INDIVIDUAL HOTEL PAGE
+  def show
+    @id = request.params[:id]
+  end
+
+
+  # FORM PAGE TO CREATE NEW HOTEL
+  def new
+    @hotel = Hotel.new()
+    @hotelCats = Hotel.get_hotel_categories
+  end
+
+
+  # CREATE METHOD
+  def create
+    @h = Hotel.create( params[:hotel] )
+    @h.save()
+  end
+
+
+  # EDIT HOTEL
+  def edit
+    h = Hotel.find( request.params[:id] )
+    @hotel = h
+    @hotelCats = Hotel.get_hotel_categories
+    @coords = { :lat => h.hotel_lat, :lng => h.hotel_lng }.to_json
+  end
+
+
+  def update
+    h = Hotel.find( params[:id] )
+    if( h.update( hotel_attributes ) )
+      flash[:success] = "#{h.hotel_name} sucessfully updated"
+      redirect_to action: :index
+    else
+      flash[:danger] = "Update unsuccessful"
+      redirect_to action: :edit
+    end
+  end
+
+
+  # DESTROY METHOD
+  def destroy
+    id = request.params[:id]
+    h = Hotel.find(id).destroy
+		flash[:warning] = "Client #{ id } was successfully deleted."
+		redirect_to action: :index
+  end
+
+
+  # CHECK TO SEE IF HOTEL NAME EXISTS
+  def checkHotel
+    h = Hotel.checkHotel( params[:hotel] )
+    if h != 0
+      render :nothing => true, :status => 409
+    else
+      render :nothing => true, :status => 200
+    end
+    return
+  end
+
 end
---
+++ db/schema.rb
@@ -0,0 +1,84 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140803002025) do
+
+  create_table "hotels", force: true do |t|
+    t.string   "hotel_name"
+    t.text     "hotel_street"
+    t.string   "hotel_city",                                 default: "Charleston"
+    t.string   "hotel_state",                                default: "SC"
+    t.integer  "hotel_zip"
+    t.string   "hotel_phone"
+    t.string   "hotel_email"
+    t.float    "hotel_lat"
+    t.float    "hotel_lng"
+    t.string   "hotel_img_url"
+    t.boolean  "hotel_pets",                                 default: false
+    t.boolean  "hotel_smoking",                              default: false
+    t.boolean  "hotel_parking",                              default: false
+    t.decimal  "hotel_pet_fee",      precision: 4, scale: 2
+    t.decimal  "hotel_smoking_fee",  precision: 4, scale: 2
+    t.string   "hotel_type"
+    t.decimal  "hotel_parking_fee",  precision: 4, scale: 2
+    t.decimal  "hotel_current_rate", precision: 6, scale: 2
+    t.string   "hotel_concierge"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rates", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reports", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reservations", force: true do |t|
+    t.integer  "rsrv_hotel_id"
+    t.string   "rsrv_creator_id"
+    t.string   "rsrv_guest_name"
+    t.string   "rsrv_guest_street"
+    t.string   "rsrv_guest_city"
+    t.string   "rsrv_guest_state"
+    t.string   "rsrv_guest_country",     default: "USA"
+    t.string   "rsrv_guest_zip"
+    t.string   "rsrv_guest_phone"
+    t.string   "rsrv_guest_email"
+    t.integer  "rsrv_guest_cc_stub"
+    t.string   "rsrv_guest_cc_exp"
+    t.integer  "rsrv_num_nights"
+    t.integer  "rsrv_num_rooms"
+    t.integer  "rsrv_num_occupants"
+    t.string   "rsrv_room_type"
+    t.boolean  "rsrv_smoking",           default: false
+    t.boolean  "rsrv_pets",              default: false
+    t.integer  "rsrv_hotel_pet_fee",     default: 0
+    t.integer  "rsrv_hotel_smoking_fee", default: 0
+    t.integer  "rsrv_hotel_parking_fee", default: 0
+    t.integer  "rsrv_extra_fees",        default: 0
+    t.integer  "rsrv_subtotal"
+    t.string   "rsrv_hotel_concierge"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
